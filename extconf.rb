4855bc41d3f48eef4ba6cb1f731e602b95a79c5b
Jose Fernandez (magec) <joseferper@gmail.com>
2011-03-29 22:46:39 +0200

    Now, glib2 and gtk2 are dependencies and used to compile

--- extconf.rb
+++ extconf.rb
@@ -1,22 +1,27 @@
 =begin
 extconf.rb for Ruby/GtkMozEmbed extention library
 =end
-
-PACKAGE_NAME = "rbwebkitgtk"
-PACKAGE_IDS = ["webkit-1.0"]
-
-TOPDIR = ENV["RG2_DIR"] || File.expand_path(File.dirname(__FILE__) + '/..')
-
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-
-SRCDIR = File.expand_path("./src")
-
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
-
+require 'rubygems'
 require 'mkmf-gnome2'
+gems_dir = File.expand_path(Gem.dir + "/gems/")
+top_dir = File.dirname(__FILE__)
+top_build_dir = File.expand_path(top_dir + "/src")
+package_name = "rbwebkitgtk"
+package_ids = ["webkit-1.0"]
+
+
+["glib2", "gtk2"].each do |package|
+  Dir.entries(gems_dir).each do |entry|
+    if /#{package}(-\d+\.\d+\.\d+)\z/ =~ entry
+      version_suffix = $1
+      $CFLAGS << ' ' + "-I #{gems_dir}/#{package}#{version_suffix}/lib"
+    end
+  end
+end
+
 
 package_id = nil
-PACKAGE_IDS.each do |v|
+package_ids.each do |v|
   if PKGConfig.exist?(v)
     package_id = v
     $stderr.puts "#{v} is found."
@@ -31,36 +36,17 @@ unless package_id
   exit 1
 end
 
+
 #
 # detect GTK+ configurations
 #
 
 PKGConfig.have_package('gtk+-2.0')
 PKGConfig.have_package(package_id)
-setup_win32(PACKAGE_NAME)
-
-# mozpath = PKGConfig.libs_only_L(package_id)
-# mozpath.strip!.sub!(/^-L/, "")
-
-# if mozpath 
-#   # please comment the CFLAGS line if you don't want a default comp_path
-#   $CFLAGS << " -DDEFAULT_MOZILLA_FIVE_HOME='\"" << mozpath << "\"' "
-#   $LDFLAGS << " -Wl,-rpath " << mozpath
-# else
-#   $stderr.puts "${package_id}.pc cannot be found."
-#   exit 1
-# end
-
-#add_depend_package("glib2", "glib/src", TOPDIR)
-#add_depend_package("gtk2", "gtk/src", TOPDIR)
-
-have_library("webkit-1.0") or exit 1
-
-# have_func('gtk_moz_embed_new') or exit 1
-# have_func('gtk_moz_embed_set_profile_path') or exit 1
 
 make_version_header("WEBKITGTK", package_id)
 
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, 
+create_makefile_at_srcdir(package_name, File.expand_path(top_dir + "/src"), 
                           "-DRUBY_WEBKITGTK_COMPILATION")
+
 create_top_makefile
